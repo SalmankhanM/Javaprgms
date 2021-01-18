2c910ab543856a7f13430cd3477b5f8afd6fd7db
SalmankhanM <salmankhan9586@gmail.com>
2021-01-18 18:39:08 +0530

    Dinesh

---
+++ src/kalaa/Salman.java
@@ -0,0 +1,18 @@
+package kalaa;
+
+public class Salman {
+	
+	public void Sal() {
+		System.out.println("Dinesh");
+
+	}
+
+	public static void main(String[] args) {
+	
+		Salman ref = new Salman();
+		ref.Sal();
+		
+
+	}
+
+}
